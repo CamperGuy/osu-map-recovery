6d80513d5158ae2d811fac10ef4caac7bcc1f02d
Joel Helbling <helblingjoel@gmail.com>
2019-05-12 15:15:32 +0100

    Polished

--- Program.cs
+++ Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Threading;
 using System.IO;
 
 namespace Osu_Map_Recovery
@@ -16,12 +17,26 @@ namespace Osu_Map_Recovery
         private static Dictionary<int, string> beatmapsTitle = new Dictionary<int, string>();
         static void Main(string[] args)
         {
+            // Console Setup
+            Console.OutputEncoding = Encoding.Unicode;
+            Console.SetWindowSize(80, 23);
+            Console.BufferWidth = Console.WindowWidth;
+            Console.BufferHeight = Console.WindowHeight;
+
             Console.Title = "Osu Map Recovery";
-            
+            Console.WriteLine("-------------------------------------------------------------------------------");
+            Console.WriteLine("||                                                                           ||");
+            Console.WriteLine("||                           Made by Joel Helbling                           ||");
+            Console.WriteLine("||               https://github.com/camperguy/osu-map-recovery               ||");
+            Console.WriteLine("||                                                                           ||");
+            Console.WriteLine("-------------------------------------------------------------------------------");
+            System.Threading.Thread.Sleep(2000);
+
+            // Request and validate the beatmap directory
             bool valid = false;
             while (!valid)
             {
-                Console.WriteLine("Please enter the current osu song directory");
+                Console.WriteLine("\nPlease enter the current osu beatmap directory");
                 Console.Write("> ");
                 string input = Console.ReadLine();
                 if (checkOsuDir(input))
@@ -31,6 +46,7 @@ namespace Osu_Map_Recovery
                 }
             }
 
+            // Request and validate the output directory
             bool valid1 = false;
             while (!valid1)
             {
@@ -44,21 +60,25 @@ namespace Osu_Map_Recovery
                 }
             }
 
-            Console.WriteLine("Grabbing songs...");
+            Console.WriteLine("\nGrabbing beatmaps...");
 
-            string[] songs = Directory.GetDirectories(sourcedir);
+            string[] beatmaps = Directory.GetDirectories(sourcedir);
             
-            Console.WriteLine("All directories have been grabbed");
+            Console.WriteLine("[x] All beatmaps have been grabbed!");
 
-            foreach (string str in songs)
+            Console.WriteLine("\nLoading beatmaps...");
+            foreach (string str in beatmaps)
             {
+                // Format the directory path
                 string fullstring = str;
                 fullstring = fullstring.Remove(0, sourcedir.Count() + 1);
 
+                // Extract the Beatmap ID
                 string[] splitID = fullstring.Split(new char[0]);
                 int ID;
                 Int32.TryParse(splitID[0], out ID);
 
+                // Extract Author and Title
                 string author = "";
                 string title = "";
                 bool pastHyphen = false;
@@ -69,44 +89,52 @@ namespace Osu_Map_Recovery
                         pastHyphen = true;
                         i++;
                     }
-
                     if (!pastHyphen)
                         author += splitID[i];
                     else
                         title += splitID[i];
                 }
 
+                // Avoid duplicates
                 if (!beatmapsAuthor.ContainsKey(ID) && !beatmapsTitle.ContainsKey(ID))
                 {
+                    // Add data to the Dictionary/HashMap
                     beatmapsAuthor.Add(ID, author);
                     beatmapsTitle.Add(ID, title);
-                    Console.WriteLine("ID: " + ID + "\nAuthor: " + author + "\nTitle: " + title+ "\n");
                 }
             }
-            Console.WriteLine("All data has been loaded");
+            Console.WriteLine("[x] All beatmaps have been loaded!");
 
+            Console.WriteLine("\nStarting to write HTML");
             writeHTML();
+            Console.WriteLine("[x] HTML file has been written!");
 
-            System.Diagnostics.Process.Start(outdir + @"\osu_songs.html");
+            System.Diagnostics.Process.Start(outdir + @"\osu_beatmaps.html");
+
+            System.Threading.Thread.Sleep(5000);
+            Environment.Exit(0);
         }
 
         static void writeHTML()
         {
-            if (!File.Exists(outdir + @"\osu_songs.html"))
+            if (!File.Exists(outdir + @"\osu_beatmaps.html"))
             {
-                Console.WriteLine("Starting to write html...");
-                StreamWriter stream = new StreamWriter(outdir + @"\osu_songs.html", true, System.Text.Encoding.UTF8);
+                
+                StreamWriter stream = new StreamWriter(outdir + @"\osu_beatmaps.html", true, System.Text.Encoding.UTF8);
 
+                // Head
                 stream.WriteLine("<html>" + stream.NewLine + @"<head>");
+                stream.WriteLine("<title>Osu Map Recovery</title>");
                 stream.WriteLine("<style>");
+                // Set up CSS
                 stream.Write("* {" + stream.NewLine + @"    font-family: sans-serif;" + stream.NewLine + "}" + stream.NewLine);
                 stream.Write("table{" + stream.NewLine + "    width: 60%; " + stream.NewLine + "    border-collapse: collapse;" + stream.NewLine + "}" + stream.NewLine);
                 stream.Write("table, th, td{" + stream.NewLine + @"    border: 1px solid black;" + stream.NewLine + "}" + stream.NewLine);
                 stream.Write("th{" + stream.NewLine + @"    background-color: #BBB;" + stream.NewLine + "}" + stream.NewLine);
                 stream.Write("tr:nth-child(odd){" + stream.NewLine + "    background-color: #DDD;" + stream.NewLine + "}" + stream.NewLine);
                 stream.Write("</style>" + stream.NewLine);
-                stream.WriteLine("<title>Osu Map Recovery</title>");
                 stream.WriteLine("</head>");
+                // Body / Table
                 stream.WriteLine("<body>");
                 stream.WriteLine("<table>");
                 stream.Write("  <tr>" + stream.NewLine + "    <th>Link</th>" + stream.NewLine + "    <th>Title</th>" + stream.NewLine + "    <th>Author</th>" + stream.NewLine + "  </tr>" + stream.NewLine);
@@ -131,7 +159,8 @@ namespace Osu_Map_Recovery
             }
             else
             {
-                File.Delete(outdir + @"\osu_maps.html");
+                Console.WriteLine("[!] Overwriting osu_beatmaps.html");
+                File.Delete(outdir + @"\osu_beatmaps.html");
                 writeHTML();
             }
         }
@@ -139,6 +168,7 @@ namespace Osu_Map_Recovery
         {
             if (Directory.Exists(path))
             {
+                // Confirm that the user has given the correct directory
                 if (File.Exists(Directory.GetParent(path) + @"\osu!.exe"))
                 {
                     return true;
@@ -148,11 +178,11 @@ namespace Osu_Map_Recovery
             }
             return false;
         }
-
         static bool checkOutDir(string path)
         {
             if (!Directory.Exists(path))
             {
+                // Ask the user if they would like to try to create their directory
                 Console.WriteLine("The directory does not exist yet.\nWould you like to create it? [y/n]");
                 if (Console.ReadKey().Key == ConsoleKey.Y)
                 {
@@ -170,6 +200,7 @@ namespace Osu_Map_Recovery
             }
             if (Directory.Exists(path))
             {
+                // Confirm that we have write access to this folder
                 try
                 {
                     Directory.GetAccessControl(path);
